4d2e9ba3749194f33da64913a4686970a7feb0d1
Iago Rosa <iagoimportantthings@gmail.com>
2024-11-12 12:55:38 -0300

    Separando por pastas e começando a main

---
+++ src/main/main.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+from menu import print_menu
+
+# Carrega as variáveis de ambiente
+load_dotenv()
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_KEY")
+
+# Cria a conexão com o Supabase
+supabase: Client = create_client(supabase_url, supabase_key)
+
+# Loop principal
+print()
+print("Bem-vindo(a) ao Banco QuemPoupaTem!!!")
+while(True):
+    print()
+    print_menu()
+    opcao = int(input("Digite uma opção: "))
+    if opcao == 1:
+        print("Gerente!")
+    elif opcao == 2:
+        print("Cliente!")
+    elif opcao == 3:
+        print("Tchau!")
+        break
+    else:
+        print("Opção inválida!")
---
+++ src/main/menu.py
@@ -0,0 +1,5 @@
+def print_menu():
+    print("Menu: ")
+    print("1. Gerente")
+    print("2. Cliente")
+    print("3. Sair")
